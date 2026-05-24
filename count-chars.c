d8bb9ded905e90f677bd5502c8cbc2a3b2f507e5
stevegpa <steven.gpa.git@gmail.com>
2026-05-23 18:24:25 -0600

    chapter 1.6 page 23

--- count-chars.c
+++ count-chars.c
@@ -2,9 +2,15 @@
 
 int main()
 {
-    long nc = 0;
-    while (getchar() != EOF)
-        ++nc;
+    // Solution A
+    // long nc = 0;
+    // while (getchar() != EOF)
+    //     ++nc;
 
-    printf("%ld\n", nc);
+    // Solution B
+    double nc;
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+
+    printf("%.0f\n", nc);
 }
---
+++ text-counting.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define IN 1    /* Inside a word */
+#define OUT 0   /* Outside a word */
+
+int main()
+{
+    int c, nl, nw, nc, state;
+    nl = nw = nc = 0;
+    state = OUT;
+    while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
+            ++nl;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
