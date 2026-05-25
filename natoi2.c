877dc5163a178409c4f8d3f48a52217f322cfc76
stevegpa <steven.gpa.git@gmail.com>
2026-05-24 19:46:39 -0600

    chapter 3 page 66

---
+++ natoi2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int natoi(char s[]);
+
+int main()
+{
+    printf("%d", natoi("150"));
+}
+
+int natoi(char s[])
+{
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); i++)
+        ;
+
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+
+    for (n = 0; isdigit(s[i]); i++)
+        n = 10 * n + (s[i] - '0');
+
+    return sign * n;
+}
