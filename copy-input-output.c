d1758f9e713c56cb509c28b770f6c5e5be80f610
stevegpa <steven.gpa.git@gmail.com>
2026-06-09 21:42:10 -0600

    chapter 8

---
+++ copy-input-output.c
@@ -0,0 +1,12 @@
+#include <_stdio.h>
+#include "syscalls.h"
+
+int main()
+{
+    char buf[BUFSIZ];
+    int n;
+
+    while ((n = read(0, buf, BUFSIZ)) > 0)
+        write(1, buf, n);
+    return 0;
+}
